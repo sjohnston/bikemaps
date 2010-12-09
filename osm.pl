ecc4519abf14ab24f2f26c3387aab589b03f64b1
Stuart Johnston <saj_git@thecommune.net>
2010-12-08 19:06:41 -0600

    Get route data from DB.

--- osm.pl
+++ osm.pl
@@ -1,41 +1,61 @@
 #!/usr/bin/perl
 
+use strict;
 use feature 'say';
 use DBI;
 use Data::Dumper;
 use LWP::UserAgent;
 use YAML qw(LoadFile);
+use JSON::XS;
 
 my $config = LoadFile('config.yaml');
 my $osm_config = $config->{'OpenStreetMap'};
 
 my $osm = osm(@{$osm_config}{qw(url user password)});
 
-my @cords = ([-96.857073053268422,32.647614612663403],[-96.873859088030812,32.647547306235538],[-96.884413445630543,32.647574753495682],[-96.89133729501394,32.647476114368764]);
+my $dbh = DBI->connect("dbi:Pg:dbname=maps", '', '', {AutoCommit => 0, RaiseError => 1});
 
-my $change = $osm->(PUT => 'changeset/create',
-'<osm>
-  <changeset>
-    <tag k="created_by" v="Perl"/>
-    <tag k="comment" v="Adding Dallas Cycle Routes"/>
-  </changeset>
-</osm>');
-my $way = qq{<osm>
- <way changeset="$change">
-   <tag k="note" v="Just a way"/>
-};
+my $rows = $dbh->selectall_arrayref(q{SELECT descriptio, ST_AsGeoJSON(the_geom) FROM onstreet WHERE groupname = 'Greater Dallas Bike Plan'});
 
-foreach my $c (@cords) {
-  my ($lon, $lat) = @$c;
-  my $node = $osm->(PUT => 'node/create', qq{<osm><node changeset="$change" lat="$lat" lon="$lon"><tag k="note" v="Just a node"/></node></osm>});
-  $way .= qq{<nd ref="$node" />\n};
+my $new_changeset = '<osm><changeset><tag k="created_by" v="Perl"/><tag k="comment" v="Adding Dallas Cycle Routes"/></changeset></osm>';
+my $change = $osm->(PUT => 'changeset/create', $new_changeset);
+
+foreach my $row (@$rows) {
+  my ($desc, $line_json) = @$row;
+  my $line = decode_json $line_json;
+  my $coords = shift @{$line->{'coordinates'}};
+
+  create_route($osm, $desc, $coords);
 }
 
-$way .= '</way></osm>';
-my $way = $osm->(PUT => 'way/create', $way);
-$osm->(PUT => 'relation/create', qq{<osm><relation changeset="$change"><tag k="note" v="Just a relation"/>member type="way" ref="$way"/></relation></osm>});
-
 $osm->(PUT => "changeset/$change/close");
+
+sub create_route {
+  my ($osm, $desc, $coords) = @_;
+
+  my ($number) = $desc =~ /(\d+)$/;
+  say "$desc:$number";
+
+  my $way = qq{<osm><way changeset="$change"><tag k="lcn_ref" v="$number"/>};
+
+  foreach my $c (@$coords) {
+    my ($lon, $lat) = @$c;
+    my $node = $osm->(PUT => 'node/create', qq{<osm><node changeset="$change" lat="$lat" lon="$lon"></node></osm>});
+    $way .= qq{<nd ref="$node" />\n};
+  }
+
+  $way .= '</way></osm>';
+  my $way = $osm->(PUT => 'way/create', $way);
+  $osm->(PUT => 'relation/create', qq{<osm><relation changeset="$change">
+  <member type="way" ref="$way"/>
+  <tag k="type" v="route"/>
+  <tag k="route" v="bicycle"/>
+  <tag k="network" v="lcn"/>
+  <tag k="ref" v="$number"/>
+  <tag k="name" v="$desc"/>
+  </relation></osm>});
+}
+
 
 sub osm {
   my ($url, $user, $password) = @_;
